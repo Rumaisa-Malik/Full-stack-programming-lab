b6705f42201c7742c62f6cda2936639f2437c618
Rumaisa-Malik <231995@students.au.edu.pk>
2026-05-06 22:16:54 +0500

    Added Lab 11 MERN Stack Project

--- Lab_11_MERN_Stack_Node_MongoDB_Lab/ecommerce-app/frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OuxW2RGGQl9MsPq3cC4DKyA0Kw6SJbJR/MheiJn8Ftg=\"\n}"
--- Lab_11_MERN_Stack_Node_MongoDB_Lab/ecommerce-app/frontend/.next/dev/static/chunks/Documents_ecommerce-app_frontend_app_layout_tsx_0olstmt._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0kfm1m3._.css","static/chunks/0l9l_next_dist_0empv.~._.js"],
-    source: "dynamic"
-});
